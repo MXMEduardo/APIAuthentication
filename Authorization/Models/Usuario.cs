bb7cb31b8df6ef065655a7de95b52d7fc9e9ae5a
Eduardo Sidnei <mxmeduardo@gmail.com>
2019-06-12 13:00:20 -0300

    Otimo exemplo de autenticação por Token nas APIs

---
+++ Authorization/Models/Usuario.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace Authorization.Models
+{
+    public class Usuario
+    {
+        public int id { get; set; }
+        public string Login { get; set; }
+        public string Senha { get; set; }
+        public string Nome { get; set; }
+        public string Role { get; set; }
+
+        public Usuario Get(string login, string senha)
+        {
+            List<Usuario> usuarios = new List<Usuario>
+            {
+                new Usuario {id = 1, Login = "MXMEduardo@gmail.com", Nome = "Eduardo", Senha = "dudu", Role = "Admin"},
+                new Usuario {id = 1, Login = "Anderson@gmail.com", Nome = "Anderson", Senha = "anderson", Role = "Usuario"},
+                new Usuario {id = 1, Login = "Valentina@gmail.com", Nome = "Valentina", Senha = "valentina", Role = "Supervisor"}
+            };
+
+            return usuarios.Find(x => x.Login == login && x.Senha == senha);
+        }
+    }
+}
---
+++ Authorization/OAuthProvider.cs
@@ -0,0 +1,52 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Owin.Security.OAuth;
+using Authorization.Models;
+using System.Collections.Generic;
+using System.Security.Claims;
+using Authorization;
+
+namespace Authorization
+{
+    public class OAuthProvider : OAuthAuthorizationServerProvider
+    {
+        public override Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            return Task.Factory.StartNew(() =>
+            {
+            string username = context.UserName;
+            string password = context.Password;
+
+            Usuario user = new Usuario().Get(username, password);
+            if (user != null)
+            {
+                List<Claim> clains = new List<Claim>
+                    {
+                        new Claim(ClaimTypes.Name, user.Nome),
+                        new Claim("UserID", user.id.ToString()),
+                        new Claim(ClaimTypes.Role, user.Role)
+                    };
+
+                ClaimsIdentity OAuthIdentity = new ClaimsIdentity(clains, Startup.OAuthOptions.AuthenticationType);
+                context.Validated(
+                       new Microsoft.Owin.Security.AuthenticationTicket(
+                           OAuthIdentity, 
+                           new Microsoft.Owin.Security.AuthenticationProperties() { } ));
+                }
+                else
+                {
+                    context.SetError("Erro", "Erro");
+                }
+            });
+        }
+
+
+        public override Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            if (context.ClientId == null)
+            {
+                context.Validated();
+            }
+            return Task.FromResult<object>(null);
+        }
+    }
+}
